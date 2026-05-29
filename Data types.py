b0114386b46fda4e4b7e59b9a1c8fcb4fbac27de
AbiramiCodingal <krish.ravi2019@gmail.com>
2026-05-29 21:45:21 +0530

    Data types

---
+++ Data types.py
@@ -0,0 +1,11 @@
+# Data types
+
+a = 34
+
+print("The type of a is",type(a))
+name = "Abirami"
+print("The type of name is",type(name))
+height = 34.5
+print("The type of height is",type(height))
+student = True
+print("The type of student is",type(student))
---
+++ String operation.py
@@ -0,0 +1,5 @@
+# reverse
+
+phrase = input("enter a phrase : ")
+print("the reverse is ...")
+print(phrase[::-1])
