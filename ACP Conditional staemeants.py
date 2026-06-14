f938656ad8e429dc036d67a6fe03d454c0ffb6ec
AbiramiCodingal <krish.ravi2019@gmail.com>
2026-06-14 18:27:57 +0530

    ACP Conditional staemeants

---
+++ ACP Conditional staemeants.py
@@ -0,0 +1,7 @@
+i = int(input("enter the temperature: "))
+if ( i < 10 ) :
+    print("it's less than 10")
+    print(" It's cold!")
+else:
+    print("it is greater than 10")
+    print("it is hot!")
