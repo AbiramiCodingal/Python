c171b3adab62eeef73631c37e23ae7c1aa52cbfb
AbiramiCodingal <krish.ravi2019@gmail.com>
2026-07-01 14:11:59 +0530

    nested loop

---
+++ nested loop.py
@@ -0,0 +1,11 @@
+# Diamond2
+space = 20
+n = int(input("enter the no of rows"))
+
+for i in range(1,n+1):
+    for k in range (1,space):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+    space-=2
